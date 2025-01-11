400ccb3263e136acab640f3d1bbd838f202c4060
Kennjenga <kinyagia10@gmail.com>
2025-01-11 22:02:59 +0300

    Remove unused font imports and set default font family to Inter in layout component

--- packages/frontend/src/app/layout.tsx
+++ packages/frontend/src/app/layout.tsx
@@ -1,20 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
 import { headers } from "next/headers";
 import { cookieToInitialState } from "wagmi";
 import { config } from "../config";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "pamoja",
@@ -34,7 +23,10 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} flex flex-col min-h-screen mx-auto max-w-[98%] sm:max-w-[94%]`}
+        className={`flex flex-col min-h-screen mx-auto max-w-[98%] sm:max-w-[94%]`}
+        style={{
+          fontFamily: "Inter",
+        }}
       >
         <Providers initialState={initialState}>{children}</Providers>
       </body>
